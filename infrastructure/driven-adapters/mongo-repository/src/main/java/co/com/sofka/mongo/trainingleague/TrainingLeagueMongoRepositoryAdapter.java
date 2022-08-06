9aeaf00ce6c20aa4c976c16b4098cc5562f20da0
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-08-06 13:51:35 -0500

    add confing of MongoRepositoryAdapter.java and coment line connect to real database in  build.gradle

---
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/mongo/trainingleague/TrainingLeagueMongoRepositoryAdapter.java
@@ -0,0 +1,31 @@
+package co.com.sofka.mongo.trainingleague;
+
+import co.com.sofka.model.trainingleague.TrainingLeagueDocument;
+import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.stereotype.Repository;
+
+import co.com.sofka.model.trainingleague.TrainingLeague;
+import co.com.sofka.model.trainingleague.gateways.TrainingLeagueRepository;
+import co.com.sofka.mongo.helper.AdapterOperations;
+import reactor.core.publisher.Mono;
+
+@Repository
+public class TrainingLeagueMongoRepositoryAdapter extends
+        AdapterOperations<TrainingLeague, TrainingLeagueDocument, String, TrainingLeagueMongoDBRepository>
+        implements TrainingLeagueRepository {
+
+    public TrainingLeagueMongoRepositoryAdapter( TrainingLeagueMongoDBRepository repository, ObjectMapper mapper) {
+        /**
+         * Could be use mapper.mapBuilder if your domain model implement builder pattern
+         * super(repository, mapper, d ->
+         * mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
+         * Or using mapper.map with the class of the object model
+         */
+        super(repository, mapper, d -> mapper.map(d, TrainingLeague.class/* change for domain model */));
+    }
+
+    @Override
+    public Mono<TrainingLeague> updateTrainingLeague(String id, TrainingLeague trainingLeague) {
+        return null;
+    }
+}
