6f3d3b3aa5e0d40a7566ab074fcab5c5a30d38d0
certhakzu <pealveco@yahoo.com>
2022-08-11 02:15:14 -0500

    cambios provisionales

--- domain/usecase/src/main/java/co/com/sofka/usecase/postulantusecase/addchallenge/AddChallengeUseCase.java
+++ domain/usecase/src/main/java/co/com/sofka/usecase/postulantusecase/addchallenge/AddChallengeUseCase.java
@@ -1,6 +1,5 @@
 package co.com.sofka.usecase.postulantusecase.addchallenge;
 
-import co.com.sofka.model.challenge.Challenge;
 import co.com.sofka.model.challenge.ChallengePostulant;
 import co.com.sofka.model.challenge.gateways.ChallengeRepository;
 import co.com.sofka.model.postulant.Postulant;
@@ -9,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
-import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -17,18 +15,10 @@ public class AddChallengeUseCase {
     private final PostulantRepository postulantRepository;
     private final ChallengeRepository challengeRepository;
 
-    public Mono<Postulant> addChallenge(String idPostulant/*, String idChallenge*/){
+    public Mono<Postulant> addChallenge(String idPostulant, String idChallenge){
         return postulantRepository.findById(idPostulant)
                 .map(postulant -> {
-                    var x = challengeRepository.findAll().map(challenge -> challenge.getId()).toStream().collect(Collectors.toList());
-                    Collections.shuffle(x);
-                    var newChallenge = new ChallengePostulant(
-                            x.get(0),
-                            postulant.getChallenge().getRegistrationDate().toString(),
-                            postulant.getChallenge().getInitialDate().toString(),
-                            postulant.getChallenge().getFinalDate().toString(),
-                            postulant.getChallenge().getLanguage());
-                    postulant.setChallenge(newChallenge);
+                    postulant.
                     return postulant;
                 }).flatMap(postulant -> postulantRepository.update(idPostulant, postulant));
     }
--- domain/usecase/src/main/java/co/com/sofka/usecase/postulantusecase/registertotraining/RegisterToTrainingUseCase.java
+++ domain/usecase/src/main/java/co/com/sofka/usecase/postulantusecase/registertotraining/RegisterToTrainingUseCase.java
@@ -2,7 +2,6 @@ package co.com.sofka.usecase.postulantusecase.registertotraining;
 
 import co.com.sofka.model.postulant.Postulant;
 import co.com.sofka.model.postulant.gateways.PostulantRepository;
-import co.com.sofka.usecase.postulantusecase.addchallenge.AddChallengeUseCase;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -10,13 +9,11 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class RegisterToTrainingUseCase {
     private final PostulantRepository postulantRepository;
-    private final AddChallengeUseCase addChallengeUseCase;
 
     public Mono<Postulant> registerToTraining(String idTraining, String idPostulant){
 
         return postulantRepository.findById(idPostulant)
                 .map(postulant -> {
-                    addChallengeUseCase.addChallenge(postulant.getId());
                     postulant.setIdTraining(idTraining);
                     return postulant;
                 }).flatMap(postulant -> postulantRepository.update(idPostulant, postulant));
